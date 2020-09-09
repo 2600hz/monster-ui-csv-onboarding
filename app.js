50bc071484338c4583e31cc13277b46234e4e015
Pilar Candia <pilar@2600hz.com>
2020-09-09 05:57:41 -0400

    Add function to set add numbers in text in the notification section

--- app.js
+++ app.js
@@ -918,7 +918,8 @@ define(function(require) {
 		},
 
 		prepareReviewData: function(data) {
-			var expected = _.get(data, 'columns.expected', []),
+			var self = this,
+				expected = _.get(data, 'columns.expected', []),
 				formattedData = {
 					data: {
 						fileName: data.fileName,
@@ -929,7 +930,8 @@ define(function(require) {
 						},
 						recordsToReview: data.records.slice(0, 5),
 						numMatches: 0,
-						totalMandatory: data.columns.expected.mandatory.length
+						totalMandatory: data.columns.expected.mandatory.length,
+						numString: ''
 					}
 				};
 
@@ -967,6 +969,9 @@ define(function(require) {
 				}
 			});
 
+			formattedData.data.numString = self.convertNumberToText(formattedData.data.numMatches);
+
+			console.log(formattedData);
 			return formattedData;
 		},
 
@@ -1033,6 +1038,29 @@ define(function(require) {
 				};
 
 			return formattedData;
+		},
+
+		convertNumberToText: function(num) {
+			var self = this,
+				a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '],
+				b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+			//add check for zero
+			if (num === 0) {
+				return self.i18n.active().csvOnboarding.review.misc.zero;
+			}
+
+			if ((num = num.toString()).length > 9) return 'overflow';
+			var n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+
+			if (!n) return; var str = '';
+			str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+			str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+			str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+			str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+			str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
+
+			return str.replace('only', '').trim();
 		},
 
 		//API endpoints
